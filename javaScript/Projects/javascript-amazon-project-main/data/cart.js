676ddb03a040eb1a7408a863decc7c1483e95bda
Hridayansh018 <hridayansh018@gmail.com>
2024-07-17 01:33:36 +0530

    Moving to the check out page

--- javaScript/Projects/javascript-amazon-project-main/data/cart.js
+++ javaScript/Projects/javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+
+
+  export function addToCart(productId) {
+    let matchingcartItem;
+  
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId)  {
+          matchingcartItem = cartItem;
+      }
+    });
+  
+    if (matchingcartItem) {
+        matchingcartItem.quantity += 1;
+    } else {
+        cart.push({productId: productId,
+        quantity: 1});
+    }
+  }
--- javaScript/Projects/javascript-amazon-project-main/scripts/amazon.js
+++ javaScript/Projects/javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,6 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
+import { products } from '../data/products.js';
+
 
 let productsHTML = '';
 
@@ -56,31 +58,21 @@ products.forEach(product => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
 
+function UpdatedCartQuantity() {
+  let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+  }
+
 document.querySelectorAll('.js-add-to-cart').forEach(button => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
 
-    let matchingItem;
-
-    cart.forEach((Item) => {
-      if (productId === Item.productId)  {
-          matchingItem = Item;
-      }
-    });
-
-    if (matchingItem) {
-        matchingItem.quantity += 1;
-    } else {
-        cart.push({productId: productId,
-        quantity: 1});
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    })
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+    addToCart(productId)
+    UpdatedCartQuantity()
   });
 })
