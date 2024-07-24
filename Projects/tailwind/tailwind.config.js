9b041be6ca121a92634cb94a56e082649896f80a
Hridayansh018 <hridayansh018@gmail.com>
2024-07-25 00:34:30 +0530

    tailwind configured successfully

---
+++ Projects/tailwind/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../**/*.{html,js}"], // Adjusted to look through all directories
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
